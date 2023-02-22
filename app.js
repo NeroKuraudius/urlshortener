7409f771adb1da64683c44aac5d92dd09b828f8f
Nero <karta0706566@gmail.com>
2023-02-22 20:22:52 +0800

    feat: add index.hbs

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 
+// 設定環境變數
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+// 連結資料庫
 mongoose.connect(process.env.MONGODB_URI)
 
 const db = mongoose.connection
@@ -15,11 +18,17 @@ db.on('error', () => {
   console.log('mongodb erroe!')
 })
 
+
 const PORT = 3000
 const app = express()
 
+//設定固定樣板 ※exphbs.engine()
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set("view engine", 'hbs')
+
+
 app.get('/', (req, res) => {
-  res.send('This is the home page')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
